a3548865c71c74d3eb3c91e234a18bd53471ab53
ASHTON JAYS <228408@andrews.esc18.net>
2026-01-30 10:40:30 -0600

    Final Commit. Learn how to implement methods.

--- StarsRunner.java
+++ StarsRunner.java
@@ -1,10 +1,10 @@
-//© A+ Computer Science
+//ï¿½ A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Ashton Jays
+//Date - 1/30/26
+//Class - CSI
+//Lab  - 
 
 import static java.lang.System.*;
 
@@ -13,7 +13,9 @@ public class StarsRunner
    public static void main(String args[])
    {
       //instantiate a StarsAndStripes object
-      
+      StarsAndStripes boxes = new StarsAndStripes();
       //call the methods needed to make the patterns on the word document
+      boxes.printASmallBox();
+      boxes.printABigBox();
    }
 }
